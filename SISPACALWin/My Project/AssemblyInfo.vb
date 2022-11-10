a92e9c8f281bd1133762e0e52e8cb38a6da856c1
Christiam Ramos <christiam.eramos@gmail.com>
2022-11-09 18:20:24 -0600

    Ajuste de la parte de sumas y 1% de retención en reporte de comprobante de retención
    A solicitud de Luis Enrique y con aval de Eunice se ajusta la parte de la cantidad en reporte de comprobante de ingreso

--- SISPACALWin/My Project/AssemblyInfo.vb
+++ SISPACALWin/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.1.184")>
-<Assembly: AssemblyFileVersion("1.0.1.184")>
+<Assembly: AssemblyVersion("1.0.1.187")>
+<Assembly: AssemblyFileVersion("1.0.1.187")>
